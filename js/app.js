7703d54fd83be45727f1ebefa5d96f30390e3395
Ismael <ismaproco@gmail.com>
2016-02-23 17:09:26 +0100

    complete basic styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,53 @@
-var app = angular.module("tigerApp", []);
+var app = angular.module("tigerApp", ['ngRoute', 'services']);
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when('/times', {
+        templateUrl: 'templates/times.html',
+        controller: 'timesController'
+    }).when('/details', {
+        templateUrl: 'templates/details.html',
+        controller: 'detailsController'
+    }).when('/', {
+        redirectTo:'/times'
+    }).otherwise({
+        templateUrl: 'templates/other.html',
+        controller: 'defaultController'
+    });
+});
+
+app.controller('navController',['$scope','$location', 'navService',function($scope, $location, navService){
+    var ctrl = this;
+    $scope.items = [];
+
+    var navigation = navService.getNavigation();
+    var navKeys = Object.keys(navigation);
+    
+    if(navKeys){
+        $scope.items = navKeys.map(function(key){
+            return navigation[key];
+        });
+    }
+
+    $scope.$location = $location;
+    $scope.$watch('$location.path()', function(newValue){
+        var value = newValue.split('/');
+        console.log(navigation);
+        navService.updateActive(value[1]);
+    });
+
+
+
+}]);
+
+app.controller('timesController',function($scope, $route, $routeParams, $location){
+    
+});
+
+app.controller('detailsController', function($scope) {
+
+});
+
+app.controller('defaultController', function($scope) {
+
+});
---
+++ js/services.js
@@ -0,0 +1,31 @@
+angular.module('services', []).service('navService', function(){
+    var nav = {
+        times: {isActive: false, name:"times", url:"#times", text:"Date and zip code",order:1},
+        details: {isActive: false, name:"details", url:"#details",text:"Cleaning Details",order:2},
+        address: {isActive: false, name:"address", url:"#address",text:"Address",order:3},
+        payment: {isActive: false, name:"payment", url:"#payment",text:"Payment",order:4}
+    };
+
+    var active = 'times';
+
+    var navManager = {
+        updateActive: function(selected){
+            if( nav[active] ) {
+                nav[active].isActive = false;
+            }
+
+            active = selected;
+
+            if(nav[active]){
+                nav[active].isActive = true;    
+                return nav[active];
+            }
+            return;
+        },
+        getNavigation: function() {
+            return nav;
+        }
+    }
+
+    return navManager;
+});
